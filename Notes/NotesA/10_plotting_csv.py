0fb6ae5bac44d2810573de2a8e8b767503ae06d8
AlexBennett <alexbennett@fwparker.org>
2020-03-13 10:03:17 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Notes/resources/super_villains.txt

--- Notes/NotesA/10_plotting_csv.py
+++ Notes/NotesA/10_plotting_csv.py
@@ -10,7 +10,18 @@ header = data.pop(0)
 print(header)
 
 print(data[0])
-names = [x[0] for x in data]
+month_names = [x[0] for x in data]
 
 sulzer_index = names.index("Sulzer Regional Library")
 print(sulzer_index)
+month_numbers = [x for x in data(12)]
+month_names = header[4:-1]
+print(month_names)
+
+plt.figure(1, tight_layout=True) # tight_fit makes everything fit in figure
+
+plt.title("Sulzer Visitors By Month")
+plt.ylabel("Visitors")
+plt.xticks(month_numbers, month_names, rotation=45)
+
+
