dd65407c5e8a9c20164dc42dfaf00f3cd80c7a45
AlexBennett <alexbennett@fwparker.org>
2020-03-23 09:09:28 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Notes/resources/super_villains.txt

--- Notes/NotesA/10_plotting_csv.py
+++ Notes/NotesA/10_plotting_csv.py
@@ -9,19 +9,40 @@ print(data)
 header = data.pop(0)
 print(header)
 
-print(data[0])
-month_names = [x[0] for x in data]
+# print(data[0])
+# month_names = [x[0] for x in data]
 
-sulzer_index = names.index("Sulzer Regional Library")
-print(sulzer_index)
-month_numbers = [x for x in data(12)]
-month_names = header[4:-1]
-print(month_names)
+# sulzer_index = names.index("Sulzer Regional Library")
+# print(sulzer_index)
+# month_numbers = [x for x in data(12)]
+# month_names = header[4:-1]
+# print(month_names)
 
-plt.figure(1, tight_layout=True) # tight_fit makes everything fit in figure
+# plt.figure(1, tight_layout=True) # tight_fit makes everything fit in figure
 
-plt.title("Sulzer Visitors By Month")
-plt.ylabel("Visitors")
-plt.xticks(month_numbers, month_names, rotation=45)
+# plt.title("Sulzer Visitors By Month", color="blue")
+# plt.ylabel("Visitors")
+# plt.xticks(month_numbers, month_names, rotation=45)
+# plt.bar(month_numbers, sultzer_by_month)
+# plt.axes([-1, 13, 3, 15]) # xin, xmax, ymin, ymax
 
+# starting over with just headers and data
 
+# plot top ten libraries for YTD totals
+plt.figure(2, tight_layout=True, figsize=(14, 6))  # fig size is how wide you want it in inches
+print(header)
+
+data.sort(key=lambda x: int(x[-1]))
+data(print)
+
+top_ten = data[-10:]
+top_ten_ytd = [int(x[-1]) for x in top_ten]
+
+top_ten_names = [x[0] for x in top_ten]
+
+x_vals = [x for x in range(len(top_ten))]
+plt.bar(x_vals, top_ten_ytd)  # barh is a horizontal bar graph
+plt.xticks(x_vals, top_ten_names, fontsize=10)
+
+plt.xlabel("Visitor YTD")
+plt.title("Top ten Most Visited Chicago Libraries")
